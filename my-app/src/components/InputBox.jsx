cadf8baf43d4a1a644c5635b62b4445143b073d8
KaiSolter <ksolter@bu.edu>
2024-02-24 00:22:00 -0500

    work on input

--- my-app/src/components/InputBox.jsx
+++ my-app/src/components/InputBox.jsx
@@ -1,12 +1,27 @@
 import { useState } from 'react';
 
+
+
 function InputBox() { 
   const [wordInput, setwordInput] = useState(''); 
   const handleinput = (event) => {
     setwordInput(event.target.value); 
   };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      verifyWord(wordInput); 
+    }
+  };
+
+  function verifyWord(word) {
+    
+    setwordInput('')
+
+}
+
   return ( 
-    <input type="text" value={wordInput} onChange={handleinput} ></input>
+    <input type="text" value={wordInput} onChange={handleinput}  onKeyDown={handleKeyDown}  ></input>
   )
 }
 
