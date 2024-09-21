43915d5343c1b799ec41f97666e6c45b66220087
Kai <kai@gem.health>
2024-09-21 16:08:41 -0400

    added assignment 1

--- src/Assignments.jsx
+++ src/Assignments.jsx
@@ -4,23 +4,42 @@ import './General.css'
 
 function Assignments() {
     return (
-        <div className="container">
-            <h1 className="black-text">Assignments</h1>
-            <div className="job">
-                <h1 className='black-text-sm'>Assignment 0</h1>
-                <div className='github-button-container' >
-                    <a
-                        href="https://github.com/KaiSolter/bu_kaisolter-assignment-0"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="github-button"
-                    >
-                        Github Link
-                    </a>
+        <>
+            <div className="container">
+                <h1 className="black-text">Assignments</h1>
+                <div className="job">
+                    <h1 className='black-text-sm'>Assignment 0</h1>
+                    <div className='github-button-container' >
+                        <a
+                            href="https://github.com/KaiSolter/bu_kaisolter-assignment-0"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="github-button"
+                        >
+                            Github Link
+                        </a>
+                    </div>
                 </div>
-            </div>
 
-        </div>
+            </div>
+            <div className="container">
+                <h1 className="black-text">Assignments</h1>
+                <div className="job">
+                    <h1 className='black-text-sm'>Assignment 1</h1>
+                    <div className='github-button-container' >
+                        <a
+                            href="https://github.com/KaiSolter/bu_kaisolter-assignment-1"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="github-button"
+                        >
+                            Github Link
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+        </>
     );
 }
 
