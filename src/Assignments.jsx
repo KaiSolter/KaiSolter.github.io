6438f5125731dac3e3c608c435567faea2c78cf5
Kai <kai@gem.health>
2024-09-21 16:22:27 -0400

    fixed error

--- src/Assignments.jsx
+++ src/Assignments.jsx
@@ -20,23 +20,6 @@ function Assignments() {
                         </a>
                     </div>
                 </div>
-
-            </div>
-            <div className="container">
-                <h1 className="black-text">Assignments</h1>
-                <div className="job">
-                    <h1 className='black-text-sm'>Assignment 0</h1>
-                    <div className='github-button-container' >
-                        <a
-                            href="https://github.com/KaiSolter/bu_kaisolter-assignment-0"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="github-button"
-                        >
-                            Github Link
-                        </a>
-                    </div>
-                </div>
                 <div className="job">
                     <h1 className='black-text-sm'>Assignment 1</h1>
                     <div className='github-button-container' >
