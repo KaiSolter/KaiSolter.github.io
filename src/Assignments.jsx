de3906e6cb99fe22d3c9e4daf739f2f0a120bf93
Kai <kai@gem.health>
2024-10-11 15:26:31 -0400

    assignment 4

--- src/Assignments.jsx
+++ src/Assignments.jsx
@@ -45,8 +45,7 @@ function Assignments() {
                             Github Link
                         </a>
                     </div>
-                </div>
-                <div className='github-button-container'>
+                    <div className='github-button-container'>
                         <a
                             href="https://youtu.be/nbONMssM0J8"
                             target="_blank"
@@ -58,6 +57,7 @@ function Assignments() {
 
                         
                     </div>
+                </div>
                     <div className="job">
                     <h1 className='black-text-sm'>Assignment 3</h1>
                     <div className='github-button-container' >
@@ -71,9 +71,32 @@ function Assignments() {
                         </a>
                     </div>
                 </div>
+                <div className="job">
+                    <h1 className='black-text-sm'>Assignment 4</h1>
+                    <div className='github-button-container' >
+                        <a
+                            href="https://github.com/KaiSolter/bu_kaisolter-assignment-4"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="github-button"
+                        >
+                            Github Link
+                        </a>
+                    </div>
+                    <div className='github-button-container'>
+                        <a
+                            href="https://youtu.be/T2ifyg9wCVo"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="github-button"
+                        >
+                            Demo Video
+                        </a>
+                        
+                    </div>
+                </div>
             </div>
         </>
     );
 }
-
 export default Assignments;
